37ce3a04f60375e6159949e5db93b84857a24e2a
moneeb-tawfeek <moneeb.tawfeek@gmail.com>
2021-02-18 02:01:17 +0200

    lab7with some prob

--- js/app.js
+++ js/app.js
@@ -6,9 +6,162 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// let Seattle = {
-//     locationName: 'seatile',
-//     mincust: 23,
+
+let resturent = [];
+function Resturent(locationName, mincust, maxcust, avgcookies) {
+    this.locationName = locationName
+    this.mincust = mincust
+    this.maxcust = maxcust
+    this.avgcookies = avgcookies
+    this.custumerperhour = [];
+    this.cookiesPressingPerHouer = []
+    this.totalcookiesperday = 0;
+    resturent.push(this)
+}
+
+
+// calculating custumer per hour
+
+Resturent.prototype.calcucustumerperhour = function () {
+    for (let i = 0; i < workinghour.length; i++) {
+        this.custumerperhour.push(Math.floor(random(this.mincust, this.maxcust)));
+        // console.log();
+    }
+}
+// custumer per hour * avg cookies give us cookies per hour
+Resturent.prototype.calcucookiesPressingPerHouer = function () {
+    for (let i = 0; i < workinghour.length; i++) {
+        this.cookiesPressingPerHouer.push(Math.floor(this.custumerperhour[i] * this.avgcookies));
+        this.totalcookiesperday += this.cookiesPressingPerHouer
+    }
+}
+
+
+let seattile = new Resturent('siattle', 23, 65, 6.3)
+
+let tokyo = new Resturent('tokyo', 3, 24, 1.2)
+
+let dubai = new Resturent('dubai', 11, 38, 3.7)
+
+let paris = new Resturent('paris', 20, 36, 2.3)
+let lima = new Resturent('lima', 23, 65, 6.3)
+
+
+console.log(resturent);
+
+
+// gitting div element
+
+let parent = document.getElementById('parent')
+console.log(parent);
+// making a table
+let table = document.createElement('table')
+parent.appendChild(table);
+// making the header row of the table 
+function makeheader() {
+    let headerRow = document.createElement('tr')
+
+    table.appendChild(headerRow)
+    // first element in the first row
+    let firsthead = document.createElement('th')
+    headerRow.appendChild(firsthead)
+    firsthead.textContent = "our shop"
+
+    // for loop to make the hour in the first row
+    for (i = 0; i < workinghour.length; i++) {
+        let tabhead = document.createElement('th')
+        headerRow.appendChild(tabhead)
+        tabhead.textContent = workinghour[i]
+    }
+    // last element in the first row
+    let lastheding = document.createElement('th')
+    headerRow.appendChild(lastheding)
+    lastheding.textContent = "daily location total"
+
+
+}
+
+// creating a function for making the name colom
+
+Resturent.prototype.resttable = function () {
+    let datarow = document.createElement('tr')
+    table.appendChild(datarow)
+
+    resName = document.createElement('td')
+    datarow.appendChild(resName)
+    resName.textContent = this.locationName;
+    for (let i = 0; i < workinghour.length; i++) {
+        let tdElement = document.createElement('td')
+        tdElement.textContent = this.cookiesPressingPerHouer[i];
+        datarow.appendChild(tdElement);
+
+    }
+    let totalDataEachShope = document.createElement('td')
+    datarow.appendChild(totalDataEachShope)
+    totalDataEachShope.textContent = this.totalcookiesperday
+
+}
+// creating a function for the footer 
+
+let makeFooter = function () {
+    let footerRow = document.createElement('tr')
+    table.appendChild(footerRow)
+    let footerTh = document.createElement('th');
+    footerRow.appendChild(footerTh);
+    footerTh.textContent = "totale"
+    let totOfTotale = 0;
+    for (let i = 0; i < workinghour.length; index++) {
+        let totaleOfHour = 0;
+        for (let j = 0; j < resturent.length; j++) {
+            totaleOfHour += resturent[j].cookiesPressingPerHouer
+            // totOfTotale += Resturent[j].cookiesPressingPerHouer[i]
+        }
+        let totale = document.createElement('th')
+        footerRow.appendChild(totale)
+        totale.textContent = totaleOfHour
+
+    }
+
+    // let finalth = document.createElement('th')
+    // footerRow.appendChild(finalth)
+    // finalth.textContent = totOfTotale
+}
+
+
+
+
+//calling functions 
+makeheader();
+
+for (i = 0; i < Resturent.length; i++) {
+    resturent[i].calcucustumerperhour();
+    resturent[i].calcucookiesPressingPerHouer();
+    resturent[i].resttable();
+}
+makeFooter();
+
+
+
+
+
+
+
+
+
+// for (i = 0; i < resturent.length; i++) {
+    //     let tabRow = document.createElement = ('tr')
+    //     table.appendChild(tabRow)
+    //     tabRow.textContent = resturent[i]
+
+    // }
+
+
+
+
+
+    // let Seattle = {
+        //     locationName: 'seatile',
+        //     mincust: 23,
 //     maxcust: 65,
 //     avgcookies: 6.3,
 //     custumerperhour: [],
@@ -270,46 +423,19 @@ function random(min, max) {
 
 
 
-let custumerperhour = [];
-let cookiesPressingPerHouer = [];
-
-function Resturent(locationName, mincust, maxcust, avgcookies) {
-    this.locationName = locationName
-    this.mincust = mincust
-    this.maxcust = maxcust
-    this.avgcookies = avgcookies
 
 
 
 
-}
+// console.log(Resturent);
 
-
-let seattile = new Resturent('siattle', 23, 65, 6.3)
-
-
-
-
-calcucookiesPressingPerHouer = function () {
-    for (let i = 0; i < workinghour.length; i++) {
-        this.cookiesPressingPerHouer.push(Math.floor(this.custumerperhour[i] * this.avgcookies));
-    }
-}
-
-
-Resturent.prototype.calcucustumerperhour = function () {
-    for (let i = 0; i < workinghour.length; i++) {
-        custumerperhour.push(Math.floor(random(this.mincust, this.maxcust)));
-        console.log();
-    }
-}
 // calcucustumerperhour();
 // lima.calcucookiesPressingPerHouer();
 // console.log(lima);
 // lima.render();
 
 
-console.log(seattile);
-console.log(Resturent);
-seattile.calcucustumerperhour();
+// console.log(seattile);
+// console.log(Resturent);
+// seattile.calcucustumerperhour();
 // seattile.calcucookiesPressingPerHouer();
